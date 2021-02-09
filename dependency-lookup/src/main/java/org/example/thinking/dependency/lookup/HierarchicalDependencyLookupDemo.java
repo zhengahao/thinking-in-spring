94c447c9d4e7a40ddbf7640eb25face45acbd180
zhengzihao <390560386@qq.com>
2021-02-09 09:57:02 +0800

    完善

--- dependency-lookup/src/main/java/org/example/thinking/dependency/lookup/HierarchicalDependencyLookupDemo.java
+++ dependency-lookup/src/main/java/org/example/thinking/dependency/lookup/HierarchicalDependencyLookupDemo.java
@@ -53,7 +53,7 @@ public class HierarchicalDependencyLookupDemo {
                 return true;
             }
         }
-        
+        return false;
     }
 
     private static void dispalyContainLocalBean(HierarchicalBeanFactory beanFactory, String beanName) {
