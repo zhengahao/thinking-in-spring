e6d14864f94162ea406386c14af3a5aa8a2d9e3c
zhengzihao <390560386@qq.com>
2021-03-27 14:10:35 +0800

    Spring Bean属性复制前阶段

--- bean-lifecycle/src/main/java/org/example/thinking/beanlifecycle/BeanInstantiationLifecycleDemo.java
+++ bean-lifecycle/src/main/java/org/example/thinking/beanlifecycle/BeanInstantiationLifecycleDemo.java
@@ -3,6 +3,8 @@ package org.example.thinking.beanlifecycle;
 import org.example.thinking.ioc.overview.domain.SuperUser;
 import org.example.thinking.ioc.overview.domain.User;
 import org.springframework.beans.BeansException;
+import org.springframework.beans.MutablePropertyValues;
+import org.springframework.beans.PropertyValues;
 import org.springframework.beans.factory.config.InstantiationAwareBeanPostProcessor;
 import org.springframework.beans.factory.support.DefaultListableBeanFactory;
 import org.springframework.beans.factory.xml.XmlBeanDefinitionReader;
@@ -55,6 +57,37 @@ public class BeanInstantiationLifecycleDemo {
                 return false;
             }
             return true;
+        }
+
+        // user是跳过Bean的属性赋值
+        // superUser是完全跳过Bean的实例化
+        @Override
+        public PropertyValues postProcessProperties(PropertyValues pvs, Object bean, String beanName) throws BeansException {
+            // 对userHolder Bean进行拦截
+            if (ObjectUtils.nullSafeEquals("userHolder", beanName) && UserHolder.class.equals(bean.getClass())) {
+
+                MutablePropertyValues propertyValues = null;
+
+                if (pvs instanceof MutablePropertyValues) {
+                    propertyValues = (MutablePropertyValues) pvs;
+                } else {
+                    propertyValues = new MutablePropertyValues();
+                }
+
+                // 假设<property name="number" value="1"/>配置的话，，那么在PropertyValues里面就包含一个PropertyValue（number=1）
+                // 等价于 <property name="number" value="1"/>配置
+                propertyValues.addPropertyValue("number", "1");
+
+                //如果存在"description"
+                if (propertyValues.contains("description")) {
+                    propertyValues.removePropertyValue("description");
+                    propertyValues.addPropertyValue("description","The user holder V2");
+                }
+
+                return propertyValues;
+            }
+
+            return null;
         }
     }
 }
--- bean-lifecycle/src/main/java/org/example/thinking/beanlifecycle/UserHolder.java
+++ bean-lifecycle/src/main/java/org/example/thinking/beanlifecycle/UserHolder.java
@@ -9,14 +9,37 @@ public class UserHolder {
 
     private final User user;
 
+    private Integer number;
+
+    private String description;
+
     public UserHolder(User user) {
         this.user = user;
+    }
+
+    public Integer getNumber() {
+        return number;
+    }
+
+    public void setNumber(Integer number) {
+        this.number = number;
+    }
+
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
     }
 
     @Override
     public String toString() {
         return "UserHolder{" +
                 "user=" + user +
+                ", number=" + number +
+                ", description='" + description + '\'' +
                 '}';
     }
 }
